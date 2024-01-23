22a25586717f414925ea42b6a87db1af45fe585a
aysuakbaba <74005918+aysuakbaba@users.noreply.github.com>
2024-01-24 01:02:17 +0300

    feat(about): added about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,63 @@
 <template>
-  <h1 class="text-center my-10">ABOUT PAGE</h1>
+  <main class="flex flex-col">
+    <section class="flex gap-8 p-10 border-b items-center">
+      <div class="w-1/2 flex items-center">
+        <img src="~/assets/images/cat-cage.png" alt="CatCage" />
+      </div>
+      <div class="w-1/2 flex items-center justify-center">
+        <h1 class="text-5xl">What is PET<span class="text-primary">FIND</span>CHAIN?</h1>
+      </div>
+    </section>
+    <section class="flex gap-8 p-10 flex-col gap-8 p-8 ">
+      <div class="flex">
+        <h3 class="text-3xl text-primary">What is PetFindChain?</h3>
+      </div>
+      <div class="w-1/2 flex items-center justify-center">
+        <p>
+          At PetFindChain, we are passionate about reuniting lost pets with
+          their loving families. Our decentralized platform is designed to make
+          the process of finding lost pets transparent, reliable, and
+          community-oriented. We believe in the power of technology to bring
+          people together for a common cause. With our innovative approach,
+          we've created a platform that harnesses the strength of community
+          collaboration to locate and bring home missing pets.
+        </p>
+      </div>
+    </section>
+    <section class="flex gap-8 p-10 flex-col items-end gap-8 p-8 ">
+      <div class="flex">
+        <h3 class="text-3xl text-primary">Our Mission</h3>
+      </div>
+      <div class="text-right flex  justify-center">
+        <div class="w-1/2 flex items-center justify-center">
+        <img src="~/assets/images/pet-care.png" alt="pet care" class="w-1/4" />
+      </div>
+        <p class="w-1/2">
+          Our mission is to provide a decentralized and efficient solution for
+          pet owners facing the heart-wrenching experience of a lost pet. By
+          leveraging blockchain technology, we ensure transparency and trust in
+          every step of the process. PetFindChain is not just a platform; it's a
+          community-driven initiative that unites pet lovers, technologists, and
+          animal welfare advocates. Together, we strive to make a positive
+          impact in the lives of both pets and their human companions.
+        </p>
+      </div>
+    </section>
+    <section class="flex gap-8 p-10 flex-col gap-8 p-8 ">
+      <div class="flex">
+        <h3 class="text-3xl text-primary">Core Values</h3>
+      </div>
+      <div class="w-1/2 flex items-center justify-center">
+
+        <ul>
+          <li><span class="font-semibold text-gray">Transparency:</span> We believe in open and honest communication to build trust within our community.</li>
+          <li><span class="font-semibold text-gray">Reliability:</span> Our platform is designed with robust technology to ensure a dependable and effective search for lost pets.</li>
+          <li><span class="font-semibold text-gray">Community-Oriented:</span> PetFindChain thrives on the strength of our community. We encourage collaboration and mutual support.</li>
+          <li><span class="font-semibold text-gray">Compassion:</span> We understand the emotional toll of losing a pet and approach every case with empathy and care.</li>
+        </ul>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script setup></script>
